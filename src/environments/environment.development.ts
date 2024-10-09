59e9072490cdd9cc1d83e34e69eaa6e4cdeb061a
oliviaSkyeHolley <o.holley.aus@gmail.com>
2024-10-09 16:24:23 +1100

    Update Enviro Files

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,30 +2,30 @@ export const environment = {
   production: false,
 
   //Authentication Endpoints
-  apiUrl: 'http://complaintdrupal.local/oauth/token',
-  csrfTokenUrl: 'http://complaintdrupal.local/session/token',
+  apiUrl: 'http://decisionsupportprojectdrupal.local/oauth/token',
+  csrfTokenUrl: 'http://decisionsupportprojectdrupal.local/session/token',
 
   //Process Endpoints
-  getProcessURL: 'http://complaintdrupal.local/rest/process/get/',
-  getProcessListURL: 'http://complaintdrupal.local/rest/process/list',
-  postProcessURL: 'http://complaintdrupal.local/rest/process/post',
-  duplicateProcessURL: 'http://complaintdrupal.local/rest/process/duplicate',
-  patchProcessURL: 'http://complaintdrupal.local/rest/process/patch/',
-  updateProcessURL:'http://complaintdrupal.local/rest/process/update/',
-  archiveProcessURL: 'http://complaintdrupal.local/rest/process/delete/',
+  getProcessURL: 'http://decisionsupportprojectdrupal.local/rest/process/get/',
+  getProcessListURL: 'http://decisionsupportprojectdrupal.local/rest/process/list',
+  postProcessURL: 'http://decisionsupportprojectdrupal.local/rest/process/post',
+  duplicateProcessURL: 'http://decisionsupportprojectdrupal.local/rest/process/duplicate',
+  patchProcessURL: 'http://decisionsupportprojectdrupal.local/rest/process/patch/',
+  updateProcessURL:'http://decisionsupportprojectdrupal.local/rest/process/update/',
+  archiveProcessURL: 'http://decisionsupportprojectdrupal.local/rest/process/delete/',
 
   //Decision Support Endpoints
-  getDecisionSupportURL: 'http://complaintdrupal.local/rest/support/get/',
-  getDecisionSupportListURL: 'http://complaintdrupal.local/rest/support/list',
-  postDecisionSupportURL: 'http://complaintdrupal.local/rest/support/post',
-  patchDecisionSupportURL: 'http://complaintdrupal.local/rest/support/update/',
-  archiveDecisionSupportURL: 'http://complaintdrupal.local/rest/support/archive/',
+  getDecisionSupportURL: 'http://decisionsupportprojectdrupal.local/rest/support/get/',
+  getDecisionSupportListURL: 'http://decisionsupportprojectdrupal.local/rest/support/list',
+  postDecisionSupportURL: 'http://decisionsupportprojectdrupal.local/rest/support/post',
+  patchDecisionSupportURL: 'http://decisionsupportprojectdrupal.local/rest/support/update/',
+  archiveDecisionSupportURL: 'http://decisionsupportprojectdrupal.local/rest/support/archive/',
 
  //Document Upload Endpoints
- fileUploadURL: 'http://complaintdrupal.local/file/upload/decision_support_file/_/file',
- postDecisionSupportDocumentsURL: 'http://complaintdrupal.local/rest/support/file/post',
- getDecisionSupportDocumentsURL: 'http://complaintdrupal.local/rest/support/file/get/',
- archiveDecisionSupportDocumentsURL: 'http://complaintdrupal.local/rest/support/file/delete/',
+ fileUploadURL: 'http://decisionsupportprojectdrupal.local/file/upload/decision_support_file/_/file',
+ postDecisionSupportDocumentsURL: 'http://decisionsupportprojectdrupal.local/rest/support/file/post',
+ getDecisionSupportDocumentsURL: 'http://decisionsupportprojectdrupal.local/rest/support/file/get/',
+ archiveDecisionSupportDocumentsURL: 'http://decisionsupportprojectdrupal.local/rest/support/file/delete/',
 
   //Client
   clientId: 'your-client-id',

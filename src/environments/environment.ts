004dafe6d2f036614b0290cd6a669b7e6ac4072b
Ashley Maher <ashley.maher@didymodesigns.com.au>
2024-11-05 13:57:59 +1100

    A few updates for production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,35 +2,35 @@ export const environment = {
   production: false,
 
   //Authenticaiton Endpoints
-  apiUrl: 'http://decisionsupportdrupal.local/oauth/token',
-  csrfTokenUrl: 'http://decisionsupportdrupal.local/session/token',
-  getUserDataUrl: 'http://decisionsupportdrupal.local/user/1?_format=json',
+  apiUrl: 'https://decisionsupportb.corporatememory.com.au/oauth/token',
+  csrfTokenUrl: 'https://decisionsupportb.corporatememory.com.au/session/token',
+  getUserDataUrl: 'https://decisionsupportb.corporatememory.com.au/user/1?_format=json',
 
   //Process Endpoints
-  getProcessURL: 'http://decisionsupportdrupal.local/rest/process/get/',
-  getProcessListURL: 'http://decisionsupportdrupal.local/rest/process/list',
-  postProcessURL: 'http://decisionsupportdrupal.local/rest/process/post',
-  duplicateProcessURL: 'http://decisionsupportdrupal.local/rest/process/duplicate',
-  patchProcessURL: 'http://decisionsupportdrupal.local/rest/process/patch/',
-  updateProcessURL:'http://decisionsupportdrupal.local/rest/process/update/',
-  archiveProcessURL: 'http://decisionsupportdrupal.local/rest/process/delete/',
+  getProcessURL: 'https://decisionsupportb.corporatememory.com.au/rest/process/get/',
+  getProcessListURL: 'https://decisionsupportb.corporatememory.com.au/rest/process/list',
+  postProcessURL: 'https://decisionsupportb.corporatememory.com.au/rest/process/post',
+  duplicateProcessURL: 'https://decisionsupportb.corporatememory.com.au/rest/process/duplicate',
+  patchProcessURL: 'https://decisionsupportb.corporatememory.com.au/rest/process/patch/',
+  updateProcessURL:'https://decisionsupportb.corporatememory.com.au/rest/process/update/',
+  archiveProcessURL: 'https://decisionsupportb.corporatememory.com.au/rest/process/delete/',
 
    //Decision Support Endpoints
-   getDecisionSupportURL: 'http://decisionsupportdrupal.local/rest/support/get/',
-   getDecisionSupportListURL:  'http://decisionsupportdrupal.local/rest/support/list',
-   postDecisionSupportURL: 'http://decisionsupportdrupal.local/rest/support/post',
-   patchDecisionSupportURL: 'http://decisionsupportdrupal.local/rest/support/update/',
-   archiveDecisionSupportURL: 'http://decisionsupportdrupal.local/rest/support/archive/',
+   getDecisionSupportURL: 'https://decisionsupportb.corporatememory.com.au/rest/support/get/',
+   getDecisionSupportListURL:  'https://decisionsupportb.corporatememory.com.au/rest/support/list',
+   postDecisionSupportURL: 'https://decisionsupportb.corporatememory.com.au/rest/support/post',
+   patchDecisionSupportURL: 'https://decisionsupportb.corporatememory.com.au/rest/support/update/',
+   archiveDecisionSupportURL: 'https://decisionsupportb.corporatememory.com.au/rest/support/archive/',
 
   //Document Upload Endpoints
-  fileUploadURL: 'http://decisionsupportdrupal.local/file/upload/decision_support_file/_/file',
-  postDecisionSupportDocumentsURL: 'http://decisionsupportdrupal.local/rest/support/file/post',
-  getDecisionSupportDocumentsURL: 'http://decisionsupportdrupal.local/rest/support/file/get/',
-  archiveDecisionSupportDocumentsURL: 'http://decisionsupportdrupal.local/rest/support/file/delete/',
+  fileUploadURL: 'https://decisionsupportb.corporatememory.com.au/file/upload/decision_support_file/_/file',
+  postDecisionSupportDocumentsURL: 'https://decisionsupportb.corporatememory.com.au/rest/support/file/post',
+  getDecisionSupportDocumentsURL: 'https://decisionsupportb.corporatememory.com.au/rest/support/file/get/',
+  archiveDecisionSupportDocumentsURL: 'https://decisionsupportb.corporatememory.com.au/rest/support/file/delete/',
 
-  getDecisionSupportReportURL: 'http://decisionsupportdrupal.local/rest/support/report/',
-  getDecisionSupportReportListURL: 'http://decisionsupportdrupal.local/rest/support/reportlist',
+  getDecisionSupportReportURL: 'https://decisionsupportb.corporatememory.com.au/rest/support/report/',
+  getDecisionSupportReportListURL: 'https://decisionsupportb.corporatememory.com.au/rest/support/reportlist',
   //Client
-  clientId: 'your-client-id',
-  clientSecret: 'your-client-secret'
+  clientId: 'j_0y13c4wFccizwCDVVVtte-ATf3lsMeHe1VyRrGbWs',
+  clientSecret: '5e9131f8fae88ae38e28d1805cbcfae93a8b60f5'
 };
